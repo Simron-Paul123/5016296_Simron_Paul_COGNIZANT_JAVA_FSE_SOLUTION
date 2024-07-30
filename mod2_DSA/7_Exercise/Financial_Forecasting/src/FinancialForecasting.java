07023660fb9852a1ac13ca466308492e96543723
Simron Paul <122999995+Simron-Paul123@users.noreply.github.com>
2024-07-30 22:42:06 +0530

    Delete mod2_DSA directory

--- mod2_DSA/7_Exercise/Financial_Forecasting/src/FinancialForecasting.java
+++
@@ -1,8 +0,0 @@
-public class FinancialForecasting{
-    public double calculateFutureValue(double currentValue, double growthRate, int years) {
-        if (years == 0) {
-            return currentValue;
-        }
-        return calculateFutureValue(currentValue * (1 + growthRate), growthRate, years - 1);
-    }
-}
