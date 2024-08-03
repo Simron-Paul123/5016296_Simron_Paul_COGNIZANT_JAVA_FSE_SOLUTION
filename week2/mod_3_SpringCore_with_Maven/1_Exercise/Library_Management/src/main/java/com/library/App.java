4f7cfae272d780bc5d1d1c902183f3390d49b267
Simron Paul <122999995+Simron-Paul123@users.noreply.github.com>
2024-08-04 02:14:53 +0530

    Add files via upload

---
+++ week2/mod_3_SpringCore_with_Maven/1_Exercise/Library_Management/src/main/java/com/library/App.java
@@ -0,0 +1,23 @@
+package com.library;
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+        @SuppressWarnings("resource")
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // Retrieve the BookService bean
+        BookService bookService = (BookService) context.getBean("bookService");
+
+        // Test the configuration
+        bookService.displayService();
+    }
+}
