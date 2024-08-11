8ba74028f62d94cc3f54c6dc26631f65b1e3ce81
Simron Paul <122999995+Simron-Paul123@users.noreply.github.com>
2024-08-11 23:08:21 +0530

    Delete week3/8_Exercise/EmployeeManagementSystem/EmployeeManagementSystem directory

--- week3/8_Exercise/EmployeeManagementSystem/EmployeeManagementSystem/src/main/java/com/employee/projection/EmployeeProjection.java
+++
@@ -1,8 +0,0 @@
-package com.employee.projection;
-
-public interface EmployeeProjection {
-    Long getId();
-    String getName();
-    String getEmail();
-    String getDepartmentName();  // Assuming you want to get the department name as well
-}
