e8267fe1872074e61714d35da3f6b5810f8c9803
Simron Paul <122999995+Simron-Paul123@users.noreply.github.com>
2024-08-24 22:52:56 +0530

    Add files via upload

---
+++ week_4&5/Exercise_7/BookstoreAPI/BookstoreAPI/src/main/java/com/bookstore/dto/Customerdto.java
@@ -0,0 +1,14 @@
+package com.bookstore.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Customerdto {
+	 private int id;
+	    private String first_name;
+	    private String last_name;
+	    private String email;
+	    private String password;
+}
